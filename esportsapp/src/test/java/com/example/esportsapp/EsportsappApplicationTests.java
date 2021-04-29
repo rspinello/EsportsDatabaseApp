4510e3bcd1615ccd1432a8b39223c682851506ed
Robert Spinello <69605507+rspinello@users.noreply.github.com>
2021-04-29 17:23:56 -0400

    Add files via upload
    Adding the new spring app to the repo

---
+++ esportsapp/src/test/java/com/example/esportsapp/EsportsappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.esportsapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EsportsappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
