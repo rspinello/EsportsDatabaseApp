a8bf6951ae76234f48deae1042dfde75392b3ff0
Jimmy <milinazzojames@gmail.com>
2021-05-02 20:16:54 -0400

    .\DatabaseManipulator.java

--- DatabaseManipulator.java
+++ DatabaseManipulator.java
@@ -1,3 +1,15 @@
 public class DatabaseManipulator {
     
+    //All the UserDao methods
+    public User selectByUserId(String userid){
+        UserDao dao = new UserDao();
+        return dao.selectByUserId(userid);
+    }
+
+    public static void main(String[] args){
+        DatabaseManipulator dm = new DatabaseManipulator();
+        //Test UserDao
+        System.out.println(dm.selectByUserId("bdugan"));
+    }
+
 }
