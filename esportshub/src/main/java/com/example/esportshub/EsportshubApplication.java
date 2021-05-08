591f95d989f8095aa58fcd02e0be3f46f65c0eee
rspinello <rspinello45@gmail.com>
2021-05-08 19:13:31 -0400

    Adding new project folder to the repo

---
+++ esportshub/src/main/java/com/example/esportshub/EsportshubApplication.java
@@ -0,0 +1,13 @@
+package com.example.esportshub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EsportshubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EsportshubApplication.class, args);
+	}
+
+}
---
+++ esportshub/src/main/java/com/example/esportshub/User.java
@@ -0,0 +1,22 @@
+package com.example.esportshub;
+
+public class User {
+    private String uname;
+    private String pass;
+
+    public String getUname() {
+        return uname;
+    }
+
+    public String getPass() {
+        return pass;
+    }
+
+    public void setName(String uname) {
+        this.uname = uname;
+    }
+
+    public void setPass(String pass) {
+        this.pass = pass;
+    }
+}
