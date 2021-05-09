882080e48fba5adde0c0b5ddb8e7b3330d746d51
Jimmy <milinazzojames@gmail.com>
2021-05-09 13:30:10 -0400

    .\Team.java

--- Team.java
+++ Team.java
@@ -13,7 +13,55 @@ public class Team{
         YEARS_PRO=y;
         MONEY_WON=mo;
     }
-
+    
+    public String getTEAMID(){
+        return TEAMID;
+    }
+    
+    public String getNAME(){
+        return NAME;
+    }
+    
+    public String getCOUNTRY(){
+        return COUNTRY;
+    }
+    
+    public int getYEARS_PRO(){
+        return YEARS_PRO;
+    }
+    
+    public double getMONEY_WON(){
+        return MONEY_WON;
+    }
+    
+    public String getMANAGERID(){
+        return MANAGERID;
+    }
+    
+    public void setTEAMID(String t){
+        TEAMID=t;
+    }
+    
+    public void setNAME(String n){
+        NAME=n;
+    }
+    
+    public void setCOUNTRY(String c){
+        COUNTRY=c;
+    }
+    
+    public void setYEARS_PRO(int y){
+        YEARS_PRO=y;
+    }
+    
+    public void setMONEY_WON(double m){
+        MONEY_WON=m;
+    }
+    
+    public void setMANAGERID(String m){
+        MANAGERID=m;
+    }
+    
     public boolean equals(Object o){
         return this.TEAMID.equals(((Team)(o)).TEAMID);
     }
