1f58a94f2ca3a3ce2da995d78ddc20f3a2bfef8a
Jimmy <milinazzojames@gmail.com>
2021-05-09 13:36:50 -0400

    .\Tournament.java

--- Tournament.java
+++ Tournament.java
@@ -10,6 +10,38 @@ public class Tournament{
         GAMEID=g;
         WINNER=w;
     }
+    
+    public String getTOURNAMENT_ID(){
+        return TOURNAMENT_ID;
+    }
+    
+    public String getDAY(){
+        return DAY;
+    }
+    
+    public String getGAMEID(){
+        return GAMEID;
+    }
+    
+    public String getWINNER(){
+        return WINNER;
+    }
+    
+    public void setTOURNAMENT_ID(String t){
+        TOURNAMENT_ID=t;
+    }
+    
+    public void setDAY(String d){
+        DAY=d;
+    }
+    
+    public void setGAMEID(String g){
+        GAMEID=g;
+    }
+    
+    public void setWINNER(String w){
+        WINNER=w;
+    }
 
     public boolean equals(Object o){
         return this.TOURNAMENT_ID.equals(((Tournament)(o)).TOURNAMENT_ID);
